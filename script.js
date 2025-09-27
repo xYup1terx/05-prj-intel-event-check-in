db835257a394a73f70cf4e8010bf8975c008313f
Amarea Temple <templeal@vcu.edu>
2025-09-27 02:12:56 +0000

    Implement form handling for check-in submission

---
+++ script.js
@@ -0,0 +1,27 @@
+//Get all needed Dom elements
+const form = document.getElementById('checkInForm');
+const nameInput = document.getElementById('attendeeName');
+const teamSelect = document.getElementById('teamSelect');
+
+//Track attendance
+let count = 0;
+const maxCount = 50;
+
+//Handle form submission
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+//Get form values
+const name = nameInput.value;
+const team = teamSelect.value;
+const teamName = teamSelect.selectedOptions[0].text;
+
+console.log(name, team, teamName);
+
+//Increment Count
+count++;
+console.log("Total check-ins: ", count );
+
+//Update progress bar
+const percentage = Math.round((count / maxCount) * 100) +"%";
+});
